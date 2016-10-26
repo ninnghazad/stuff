04e3da3140ff6291150f1b6ac7eb39792f3e7e5e
ninnghazad <ninnghazad@users.noreply.github.com>
2016-10-26 16:46:11 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -11,8 +11,8 @@
   spec.epsilon = 0.2; // initial epsilon for epsilon-greedy policy, [0, 1)
   spec.alpha = 0.005; // value function learning rate
   spec.experience_add_every = 5; // number of time steps before we add another experience to replay memory
-  spec.experience_size = 10000; // size of experience
-  spec.learning_steps_per_iteration = 5;
+  spec.experience_size = 100000; // size of experience
+  spec.learning_steps_per_iteration = 50;
   spec.tderror_clamp = 1.0; // for robustness
   spec.num_hidden_units = 64*64 // number of neurons in hidden layer
 
