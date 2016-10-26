be28b301967bf48a9b309ad1ba947e87ff0da305
ninnghazad <ninnghazad@users.noreply.github.com>
2016-10-26 14:54:59 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -5,20 +5,21 @@
   $.getScript('http://cs.stanford.edu/people/karpathy/convnetjs/build/convnet-min.js', function()
   {
       console.log("convnet loaded");
-      brain = new deepqlearn.Brain(64, 64); // 3 inputs, 2 possible outputs (0,1)
-  
-    });
+      brain = new deepqlearn.Brain(64, 64);  
+      console.log("brain created");
+  });
+  /*
   var state = [Math.random(), Math.random(), Math.random()];
   for(var k=0;k<10000;k++) {
       var action = brain.forward(state); // returns index of chosen action
       var reward = action === 0 ? 1.0 : 0.0;
       brain.backward([reward]); // <-- learning magic happens here
       state[Math.floor(Math.random()*3)] += Math.random()*2-0.5;
-  }
+  }*/
   brain.epsilon_test_time = 0.0; // don't make any more random choices
   brain.learning = false;
   // get an optimal action from the learned policy
-  var action = brain.forward(array_with_num_inputs_numbers);
+  //var action = brain.forward(array_with_num_inputs_numbers);
   
   function sum(ns) {
     return ns.reduce(function (t, n) {return t + n;});
