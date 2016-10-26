33fbbc630483588654f4102eaf62a481150a358a
ninnghazad <ninnghazad@users.noreply.github.com>
2016-10-26 16:37:23 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -5,8 +5,17 @@
   env.getNumStates = function() { return 64; }
   env.getMaxNumActions = function() { return 64; }
 
-  // create the DQN agent
-  var spec = { alpha: 0.01 } // see full options on DQN page
+  var spec = {}
+  spec.update = 'qlearn'; // qlearn | sarsa
+  spec.gamma = 0.9; // discount factor, [0, 1)
+  spec.epsilon = 0.2; // initial epsilon for epsilon-greedy policy, [0, 1)
+  spec.alpha = 0.005; // value function learning rate
+  spec.experience_add_every = 5; // number of time steps before we add another experience to replay memory
+  spec.experience_size = 10000; // size of experience
+  spec.learning_steps_per_iteration = 5;
+  spec.tderror_clamp = 1.0; // for robustness
+  spec.num_hidden_units = 100 // number of neurons in hidden layer
+
   var agent;
   
 
