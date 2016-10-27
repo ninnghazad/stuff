f78a459428044e47f7dfa66cb6d0dde86c468674
ninnghazad <ninnghazad@users.noreply.github.com>
2016-10-27 09:27:32 +0200

    Update ai.js

--- ai.js
+++ ai.js
@@ -14,12 +14,12 @@
   spec.update = 'qlearn'; // qlearn | sarsa
   spec.gamma = 0.9; // discount factor, [0, 1)
   spec.epsilon = 0.2; // initial epsilon for epsilon-greedy policy, [0, 1)
-  spec.alpha = 0.1; // value function learning rate
-  spec.experience_add_every = 5; // number of time steps before we add another experience to replay memory
-  spec.experience_size = 1000; // size of experience
-  spec.learning_steps_per_iteration = 5;
+  spec.alpha = 0.005; // value function learning rate
+  //spec.experience_add_every = 5; // number of time steps before we add another experience to replay memory
+  spec.experience_size = 10000; // size of experience
+  spec.learning_steps_per_iteration = 20;
   spec.tderror_clamp = 1.0; // for robustness
-  spec.num_hidden_units = 64*3 // number of neurons in hidden layer
+  spec.num_hidden_units = 64+3 // number of neurons in hidden layer
 
   var agent;
   var counter = 0;
